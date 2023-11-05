d174b30cf4e837d8a3af83d1d99939043f9061e8
hari-rajan-2499 <harirajan2499@gmail.com>
2023-11-05 19:31:22 +0530

    added card slider section using swiper

--- myjs.js
+++ myjs.js
@@ -1,7 +1,45 @@
-const mymenubar=document.querySelector('#myicondiv');
-const myullist=document.querySelector('.ul-links');
-const mybodyy=document.querySelector('body');
-mymenubar.addEventListener('click',()=>{
+const mymenubar = document.querySelector('#myicondiv');
+const myullist = document.querySelector('.ul-links');
+const mybodyy = document.querySelector('body');
+mymenubar.addEventListener('click', () => {
     myullist.classList.toggle('addul')
     mybodyy.classList.toggle('byaxis')
 })
+
+
+
+/********************* Swiper js  **************************/
+let swiperCards = new Swiper('.swipercard-js-style', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets:true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 2,
+        },
+        900: {
+            slidesPerView: 3,
+        },
+        1100: {
+            slidesPerView: 4,
+        },
+    },
+
+
+});
+
+
