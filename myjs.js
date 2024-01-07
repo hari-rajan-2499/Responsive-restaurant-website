9c5cba3785670a931a6d3d568c11316675145863
hari-rajan-2499 <harirajan2499@gmail.com>
2024-01-07 15:20:37 +0530

    modified windowload time in order-online-page

--- myjs.js
+++ myjs.js
@@ -1,9 +1,11 @@
 const mymenubar = document.querySelector('#myicondiv');
 const myullist = document.querySelector('.ul-links');
 const mybodyy = document.querySelector('body');
+
 mymenubar.addEventListener('click', () => {
     myullist.classList.toggle('addul')
     mybodyy.classList.toggle('byaxis')
+    
 })
 
 let nav_atag = document.querySelectorAll(".ul-links li a");
